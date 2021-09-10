4d4ffec8c8d935784a953bfea63f01538e893558
dovigod <inaki96@naver.com>
2021-09-10 17:58:13 +0900

    error fixed, polishing

--- src/App.js
+++ src/App.js
@@ -1,50 +1,53 @@
 
 
 const FoodList = [{
+  id: 1,
   name: "Kimchi",
   image:
     "http://aeriskitchen.com/wp-content/uploads/2008/09/kimchi_bokkeumbap_02-.jpg"
 },
 {
+  id: 2,
   name: "Samgyeopsal",
   image:
     "https://3.bp.blogspot.com/-hKwIBxIVcQw/WfsewX3fhJI/AAAAAAAAALk/yHxnxFXcfx4ZKSfHS_RQNKjw3bAC03AnACLcBGAs/s400/DSC07624.jpg"
 },
 {
+  id: 3,
   name: "Bibimbap",
   image:
     "http://cdn-image.myrecipes.com/sites/default/files/styles/4_3_horizontal_-_1200x900/public/image/recipes/ck/12/03/bibimbop-ck-x.jpg?itok=RoXlp6Xb"
 },
 {
+  id: 4,
   name: "Doncasu",
   image:
     "https://s3-media3.fl.yelpcdn.com/bphoto/7F9eTTQ_yxaWIRytAu5feA/ls.jpg"
 },
 {
+  id: 5,
   name: "Kimbap",
   image:
     "http://cdn2.koreanbapsang.com/wp-content/uploads/2012/05/DSC_1238r-e1454170512295.jpg"
 }]
 
-const Food = ({name , image}) =>{
-  console.log(1)
-  console.log(name)
-  return <div class="foodItem">
+const Food = ({id, name , image}) =>{
+  
+  return <div className="foodItem">
     <img src={image} alt={`food::${name}`}/>
     <h3> I Love components {name}!!</h3>
   </div>
   
 }
-
+const foodRenderer = (food) =>{
+    return <Food key={food.id} name={food.name} image={food.image}/>
+}
 function App() {
   return (
     <>
     <div className="App">
     </div>
-    {FoodList.map(food => {
-      // ** warning
-      return <Food name={food.name} image={food.image}/>
-    })}
+    {FoodList.map(foodRenderer)}
     </>
     
   );
@@ -53,5 +56,7 @@ function App() {
 export default App;
 
 /* note
-dynamic data applying to props
+polishing,
+index.js:1 Warning: Each child in a list should have a unique "key" prop.
+--> need private key value , if component pushed to list, it loses its originality
 */
