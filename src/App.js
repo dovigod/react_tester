ed6f14d3985e090c2644663ee977f6cd3e128d09
dovigod <inaki96@naver.com>
2021-09-10 17:25:19 +0900

    props magic

--- src/App.js
+++ src/App.js
@@ -1,14 +1,27 @@
-import ExComponent from './ExComponent';
+
+
+
+const Tester = (props) =>{
+  console.log(props)
+  return <h3> I Love components!!</h3>
+}
 
 function App() {
   return (
     <>
     <div className="App">
     </div>
-    <ExComponent></ExComponent>
+    <Tester name="jisang" potato = {true} someThing ={[1,2,3,4,5,false]}/>
     </>
     
   );
 }
 
 export default App;
+
+/* note
+how to send data to child component
+ name="jisang"
+ name => prop
+ 'jisnag' = value
+*/
--- src/ExComponent.js
+++
@@ -1,9 +0,0 @@
-const ExComponent = () =>{
-    return <span>
-        I love component
-    </span>
-
-}
-//must be capital letter
-
-export default  ExComponent;
