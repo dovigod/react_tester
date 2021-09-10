5f489088f8969d558e0834bbab70d0851bada2c6
dovigod <inaki96@naver.com>
2021-09-10 19:09:37 +0900

    build up....

--- src/App.js
+++ src/App.js
@@ -3,8 +3,24 @@ import React from 'react';
 import FoodList from './foodlist.js';
 
 class App extends React.Component {
+	state = {
+		isLoading: true,
+		movies: []
+	};
+
+	componentDidMount = () => {
+		setTimeout(() => {
+			this.setState({ isLoading: false });
+		}, 2000);
+	};
 	render() {
-		return <></>;
+		const { isLoading } = this.state;
+
+		return (
+			<>
+				<div>{!isLoading ? 'We are ready' : 'loading...'}</div>
+			</>
+		);
 	}
 }
 export default App;
