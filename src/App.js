a300b6471171166157067b0c1d75ab6fddd118cd
dovigod <inaki96@naver.com>
2021-09-12 17:48:54 +0900

    details

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import { HashRouter, Route } from 'react-router-dom';
+import Navigation from './components/Navigation';
 import About from './routes/About';
 import Home from './routes/Home';
+import Detail from './routes/Detail';
+
 const App = () => {
 	return (
 		<HashRouter>
+			<Navigation />
 			<Route path="/" exact={true} component={Home} />
 			<Route path="/about" component={About} />
+			<Route path="/movie-detail" component={Detail} />
 		</HashRouter>
 	);
 };
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -1,25 +1,39 @@
 import PropTypes from 'prop-types';
 import './Movie.css';
+import { Link } from 'react-router-dom';
 //doesn't need state
 
 const Movie = ({ id, title, summary, rating, poster, year, runTime, genres }) => {
 	return (
 		<div className="movie">
-			<img className="movie_poster" src={poster}></img>
-			<div className="movie_data">
-				<h3 className="movie_title">{title}</h3>
-				<ul className="movie_genres">
-					{genres.map((genre, idx) => {
-						if (idx == genres.length - 1) {
-							return <li className="genre">{genre}</li>;
-						}
-						return <li className="genre">{genre},</li>;
-					})}
-				</ul>
-				<h4 className="movie_year"> {year}</h4>
-				<h3 className="movie_rating">{rating}</h3>
-				<h5 className="movie_summary">{summary.slice(0, 180)}...</h5>
-			</div>
+			<Link
+				to={{
+					pathname: '/movie-detail',
+					state: {
+						year,
+						title,
+						summary,
+						poster,
+						genres
+					}
+				}}
+			>
+				<img className="movie_poster" src={poster}></img>
+				<div className="movie_data">
+					<h3 className="movie_title">{title}</h3>
+					<ul className="movie_genres">
+						{genres.map((genre, idx) => {
+							if (idx == genres.length - 1) {
+								return <li className="genre">{genre}</li>;
+							}
+							return <li className="genre">{genre},</li>;
+						})}
+					</ul>
+					<h4 className="movie_year"> {year}</h4>
+					<h3 className="movie_rating">{rating}</h3>
+					<h5 className="movie_summary">{summary.slice(0, 180)}...</h5>
+				</div>
+			</Link>
 		</div>
 	);
 };
